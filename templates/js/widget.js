56675f1b2312cb81a03748363bed168fe44095fa
rodnover <rodnover55@gmail.com>
2012-10-16 13:58:56 +0700

    Change class widget header

--- templates/js/widget.js
+++ templates/js/widget.js
@@ -3,8 +3,8 @@
 		setInterval(function(){
 			$.post('/wp-admin/admin-ajax.php', {action: 'last_track_shoutcast', rnd: Math.random()},
 				function(response) {
-					$('#last_track_shoutcast').children('.widget-title').html(response.title);
-					$('#last_track_shoutcast').children('.widget-title').next().
+					$('#last_track_shoutcast').children('.widgettitle').html(response.title);
+					$('#last_track_shoutcast').children('.widgettitle').next().
 						html(response.content);
 				}, 'json');
 			}, 60000);
