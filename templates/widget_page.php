481db708fce50213ba27f417d16f254d0b7bd85c
rodnover <rodnover55@gmail.com>
2012-10-16 16:39:31 +0700

    Correct widget tempate

--- templates/widget_page.php
+++ templates/widget_page.php
@@ -19,22 +19,24 @@
     }
     else {?>
       <div>
-        <div class="current_song_title"><?php echo $current_song; ?></div>
-          <?php
-            text_with_link($href,
-              "<div class='current_song_artist'>{$songs['current']['artist']}</div>"
-              . "<div class='current_song_track'>{$songs['current']['track']}</div>"); ?><br/>
-      </div>
-      <div><div class="last_songs_title"><?php echo $last_songs; ?></div>
-        <ul>
-        <?php foreach ($songs['lasts'] as $song) { ?>
-          <li>
-            <div class="last_song_artist"><?php echo $song['artist']; ?></div>
-            <div class="last_song_track"><?php echo $song['track']; ?></div>
-          </li>
-          <?php
-        } ?>
-        </ul>
+        <div>
+          <div class="current_song_title"><?php echo $current_song; ?></div>
+            <?php
+              text_with_link($href,
+                "<div class='current_song_artist'>{$songs['current']['artist']}</div>"
+                . "<div class='current_song_track'>{$songs['current']['track']}</div>"); ?><br/>
+        </div>
+        <div><div class="last_songs_title"><?php echo $last_songs; ?></div>
+          <ul>
+          <?php foreach ($songs['lasts'] as $song) { ?>
+            <li>
+              <div class="last_song_artist"><?php echo $song['artist']; ?></div>
+              <div class="last_song_track"><?php echo $song['track']; ?></div>
+            </li>
+            <?php
+          } ?>
+          </ul>
+        </div>
       </div>
       <?php
     }?>
