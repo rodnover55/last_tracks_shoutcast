553c3e86deb4aaf9faf951793acca5c59f0376e3
rodnover <rodnover55@gmail.com>
2012-10-16 13:32:39 +0700

    fixed: cached ajax-request

--- templates/js/widget.js
+++ templates/js/widget.js
@@ -1,7 +1,7 @@
 (function($){
 	$(function() {
 		setInterval(function(){
-			$.post('wp-admin/admin-ajax.php', {action: 'last_track_shoutcast'},
+			$.post('/wp-admin/admin-ajax.php', {action: 'last_track_shoutcast', rnd: Math.random()},
 				function(response) {
 					$('#last_track_shoutcast').children('.widget-title').html(response.title);
 					$('#last_track_shoutcast').children('.widget-title').next().
